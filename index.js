2e6fac5bcd7b1e07cba0a65b9c2266b7ee726f2d
owen849600948 <849600948@qq.com>
2017-09-05 22:48:45 +0800

    测试

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var MiniRefreshTools = require('./dist/debug/minirefresh.js');
+var MiniRefreshTools = require('./dist/debug/minirefresh.js');//测试
 
 module.exports = MiniRefreshTools;
+module.exports = MiniRefreshTools;//这样
+//火狐
+//谷歌
